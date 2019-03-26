61aebbc1d1bed3f80bd25d86f6c1499466033b4f
BettyButcher <betty@torncentral.com>
2019-03-26 13:36:17 -0500

    Add files via upload
    .Config File  for SQL connect

---
+++ .config.php
@@ -0,0 +1,8 @@
+<?php
+# main database
+define('SQL_HOST', 'db.host');
+define('SQL_USER', 'username');
+define('SQL_PASS', 'password');
+define('SQL_DATA', 'db.database');
+define('SQLDIRECT_PASSWORD', 'directpassword');
+?>
