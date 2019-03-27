67995bc237a95c01428f04eba9e393171d2d5e9e
BettyButcher <betty@torncentral.com>
2019-03-27 00:46:20 -0500

    Add files via upload
    Replacing Counts with data insert for table.

--- sql_table/counts.sql
+++ sql_table/counts.sql
@@ -1,5 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: db372387928.db.1and1.com
+-- Generation Time: Mar 27, 2019 at 05:45 AM
+-- Server version: 5.5.60-0+deb7u1-log
+-- PHP Version: 7.0.33-0+deb9u3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db372387928`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `counts`
+--
+
 CREATE TABLE `counts` (
- `vkey` varchar(12) COLLATE latin1_general_ci NOT NULL,
- `value` int(11) NOT NULL,
- PRIMARY KEY (`vkey`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci
+  `vkey` varchar(12) COLLATE latin1_general_ci NOT NULL,
+  `value` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `counts`
+--
+
+INSERT INTO `counts` (`vkey`, `value`) VALUES
+('m', 2298166),
+('update2', 331956),
+('i', 1052223),
+('c', 1091812),
+('h', 1095509),
+('u', 1339638),
+('s', 990105),
+('a', 1299833),
+('j', 1131377),
+('x', 1184059),
+('e', 1135094),
+('z', 1179496);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `counts`
+--
+ALTER TABLE `counts`
+  ADD PRIMARY KEY (`vkey`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
